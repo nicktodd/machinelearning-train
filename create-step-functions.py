2e81dbcade875eb1036a8d1bc9a6862fd2eb0cf5
Nick Todd <nick.todd@conygre.com>
2020-12-26 14:21:25 +0000

    Complete pipeline including wait

--- create-step-functions.py
+++ create-step-functions.py
@@ -315,15 +315,15 @@ workflow.update(
 training_job_name = "customer_church_training_job" + dateAsString
 
 # Finally, run the workflow!
-#execution = workflow.execute(
-#    inputs={
-#        'TrainingJobName': training_job_name, # Each Sagemaker Job requires a unique name,
-#        'ModelName': model_name # Each Model requires a unique name,   
-#    }
-#)
+execution = workflow.execute(
+    inputs={
+        'TrainingJobName': training_job_name, # Each Sagemaker Job requires a unique name,
+        'ModelName': model_name # Each Model requires a unique name,   
+   }
+)
 
 # can be commented out to avoid unnecessary costs of keeping build container alive
-#execution.wait_for_completion()
+execution.wait_for_completion()
 
 
 # now let's create the cloudformation template parameters file ready for the CodeDeploy step in the pipeline
